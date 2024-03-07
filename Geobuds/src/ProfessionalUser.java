69d11709e528b4bfbd7255c7f17e31c5e2c90102
Enzo Fernandez <127005087+enzonator4@users.noreply.github.com>
2024-03-07 00:16:58 -0500

    Update ProfessionalUser.java

--- Geobuds/src/ProfessionalUser.java
+++ Geobuds/src/ProfessionalUser.java
@@ -1,1 +1,12 @@
 
+import java.util.ArrayList;
+
+public class ProfessionalUser extends User{
+  private String affiliation;
+  private
+
+  public ProfessionalUser(String name, int uid, String[] bio, double longitude, double latitude, ArrayList<User> blockedUsers) {
+    super(name, uid, bio, longitude, latitude, blockedUsers);
+  }
+
+}
