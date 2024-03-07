08e6345d0365ca6248219e7f340bc7c16425763a
Enzo Fernandez <enzofernandez@Enzos-MacBook-Air.local>
2024-03-06 23:41:02 -0500

    first commit

--- Geobuds/src/User.java
+++ Geobuds/src/User.java
@@ -1,12 +1,13 @@
+import java.util.ArrayList;
 public class User {
     private String name;
     private int uid;
     private String[] bio;
     private double longitude;
     private double latitude;
-    private Object[] blockedUsers;
+    private ArrayList<User> blockedUsers;
 
-    public User(String name, int uid, String[] bio, double longitude, double latitude, Object[] blockedUsers) {
+    public User(String name, int uid, String[] bio, double longitude, double latitude, ArrayList<User> blockedUsers) {
         this.name = name;
         this.uid = uid;
         this.bio = bio;
@@ -14,5 +15,98 @@ public class User {
         this.latitude = latitude;
         this.blockedUsers = blockedUsers;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public String[] getBio() {
+        return bio;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public ArrayList<User> getBlockedUsers() {
+        return blockedUsers;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public void setBio(String[] bio) {
+        this.bio = bio;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setBlockedUsers(ArrayList<User> blockedUsers) {
+        this.blockedUsers = blockedUsers;
+    }
+
+    public boolean updateBio(){
+        return true;
+    }
+
+    public boolean updateName(){
+        return true;
+    }
+
+    public boolean logIn(){
+        return true;
+    }
+
+    public boolean logOut(){
+        return true;
+    }
+
+    public boolean updateProfilePicture(){
+        return true;
+    }
+
+    public boolean messageUser(){
+        return true;
+    }
+
+    public boolean receiveMessage(){
+        return true;
+    }
+
+    public boolean blockUser(){
+        return true;
+    }
+
+    public boolean unblockUser(){
+        return true;
+    }
+
+    public boolean post(){
+        return true;
+    }
+
+    public boolean deletePost(){
+        return true;
+    }
+
 
 }
