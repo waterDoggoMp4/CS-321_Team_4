8502a52b36192ab0c77e79689befbcf241efa619
CBHwang010 <116900899+CBHwang010@users.noreply.github.com>
2024-03-06 23:32:19 -0500

    Update User.java
    Added getter and setter methods

--- Geobuds/src/User.java
+++ Geobuds/src/User.java
@@ -15,4 +15,44 @@ public class User {
         this.blockedUsers = blockedUsers;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public String[] getBio() {
+        return bio;
+    }
+    
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Object[] getBlockedUsers() {
+        return blockedUsers;
+    }
+
+    public void setBlockedUsers(Object[] blockedUsers) {
+        this.blockedUsers = blockedUsers;
+    }
+
 }
