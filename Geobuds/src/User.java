e2e2e769ea43d6ab306865c893efd0367649427f
Enzo Fernandez <enzofernandez@Enzos-MacBook-Air.local>
2024-03-06 23:47:43 -0500

    added methods

--- Geobuds/src/User.java
+++ Geobuds/src/User.java
@@ -1,12 +1,13 @@
+import java.util.ArrayList;
 public class User {
     private String name;
     private int uid;
     private String[] bio;
     private double longitude;
     private double latitude;
-    private Object[] blockedUsers;
+    private ArrayList<User> blockedUsers;
 
-    public User(String name, int uid, String[] bio, double longitude, double latitude, Object[] blockedUsers) {
+    public User(String name, int uid, String[] bio, double longitude, double latitude, ArrayList<User> blockedUsers) {
         this.name = name;
         this.uid = uid;
         this.bio = bio;
@@ -19,10 +20,13 @@ public class User {
         return name;
     }
 
+<<<<<<< HEAD
     public void setName(String name) {
         this.name = name;
     }
 
+=======
+>>>>>>> 08e6345 (first commit)
     public int getUid() {
         return uid;
     }
@@ -30,23 +34,54 @@ public class User {
     public String[] getBio() {
         return bio;
     }
+<<<<<<< HEAD
     
+=======
+
+>>>>>>> 08e6345 (first commit)
     public double getLongitude() {
         return longitude;
+    }
+
+<<<<<<< HEAD
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+=======
+>>>>>>> 08e6345 (first commit)
+    public double getLatitude() {
+        return latitude;
+    }
+
+<<<<<<< HEAD
+=======
+    public ArrayList<User> getBlockedUsers() {
+        return blockedUsers;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public void setBio(String[] bio) {
+        this.bio = bio;
     }
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
     }
 
-    public double getLatitude() {
-        return latitude;
-    }
-
+>>>>>>> 08e6345 (first commit)
     public void setLatitude(double latitude) {
         this.latitude = latitude;
     }
 
+<<<<<<< HEAD
     public Object[] getBlockedUsers() {
         return blockedUsers;
     }
@@ -55,4 +90,55 @@ public class User {
         this.blockedUsers = blockedUsers;
     }
 
+=======
+    public void setBlockedUsers(ArrayList<User> blockedUsers) {
+        this.blockedUsers = blockedUsers;
+    }
+
+    public boolean updateBio(){
+        return true;
+    }
+
+    public boolean updateName(){
+        return true;
+    }
+
+    public boolean logIn(){
+        return true;
+    }
+
+    public boolean logOut(){
+        return true;
+    }
+
+    public boolean updateProfilePicture(){
+        return true;
+    }
+
+    public boolean messageUser(){
+        return true;
+    }
+
+    public boolean receiveMessage(){
+        return true;
+    }
+
+    public boolean blockUser(){
+        return true;
+    }
+
+    public boolean unblockUser(){
+        return true;
+    }
+
+    public boolean post(){
+        return true;
+    }
+
+    public boolean deletePost(){
+        return true;
+    }
+
+
+>>>>>>> 08e6345 (first commit)
 }
