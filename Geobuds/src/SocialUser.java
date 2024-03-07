c5461643c11d99377d6401cbe0a11514a5561a5d
JasonJones98 <158085901+JasonJones98@users.noreply.github.com>
2024-03-06 23:39:06 -0500

    Update SocialUser.java

--- Geobuds/src/SocialUser.java
+++ Geobuds/src/SocialUser.java
@@ -1,1 +1,17 @@
-p
+import java.util.ArrayList;
+
+public class SocialUser extends User{
+  private ArrayList<User> friendsList;
+
+  private boolean addFriend(User user){
+    
+  }
+
+  private boolean removeFriend(User user){
+    
+  }
+
+  private User getFriend(int userID){
+    
+  }
+}
