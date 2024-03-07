c0f98a9450507ffd359c362dc8e98930f0adfcc5
Enzo Fernandez <enzofernandez@Enzos-MacBook-Air.local>
2024-03-07 00:07:51 -0500

    first commit

--- Geobuds/src/SocialUser.java
+++ Geobuds/src/SocialUser.java
@@ -4,14 +4,14 @@ public class SocialUser extends User{
   private ArrayList<SocialUser> friendsList;
 
   private boolean addFriend(SocialUser user){
-    
+    return true;
   }
 
   private boolean removeFriend(SocialUser user){
-    
+    return true;
   }
 
   private User getFriend(int userID){
-    
+    return null;
   }
 }
